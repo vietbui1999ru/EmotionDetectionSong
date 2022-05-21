f35c94a6f3e8e050316b00c7ef7064b5ff67bee6
vietbui1999ru <buiquocviet99@gmail.com>
2022-05-21 15:07:16 -0500

    add files to repo

---
+++ helpers.py
@@ -0,0 +1,153 @@
+import spotipy
+import time
+from IPython.core.display import clear_output
+from spotipy import SpotifyClientCredentials, util
+
+
+"""
+This file contains functions to pull requests from Spotify Web API:
+Get album ids
+Get album songs ids
+Get all song's features
+Get neccessary song's features for Machine Learning
+Get song artist's id's playlist
+Download albums
+Download playlists
+
+"""
+
+"""
+Connect with Spotify
+using user credentials
+"""
+client_id='a191959864a749c1bcf5e67c9aeb3880'
+client_secret='6a969123034245e68d144be6c84db87f'
+redirect_uri='http://localhost:1999/'
+username = '21zfnefkptclwec56htel2eei'
+scope = 'playlist-modify-public'
+
+#Credentials to access the Spotify Music Data
+manager = SpotifyClientCredentials(client_id,client_secret)
+sp = spotipy.Spotify(client_credentials_manager=manager)
+
+#Credentials to access to  the Spotify User's Playlist, Favorite Songs, etc. 
+token = util.prompt_for_user_token(username,scope,client_id,client_secret,redirect_uri) 
+spt = spotipy.Spotify(auth=token)
+
+#get the ids of albums
+def get_albums_id(ids):
+    album_ids = []
+    results = sp.artist_albums(ids)
+    for album in results['items']:
+        album_ids.append(album['id'])
+    return album_ids
+
+#get song ids in the albums
+def get_album_songs_id(ids):
+    song_ids = []
+    results = sp.album_tracks(ids,offset=0)
+    for songs in results['items']:
+        song_ids.append(songs['id'])
+    return song_ids
+
+#get song features of songs
+#audio signatures such as 
+def get_songs_features(ids):
+
+    meta = sp.track(ids)
+    features = sp.audio_features(ids)
+
+    # meta data
+    name = meta['name']
+    album = meta['album']['name']
+    artist = meta['album']['artists'][0]['name']
+    release_date = meta['album']['release_date']
+    length = meta['duration_ms']
+    popularity = meta['popularity']
+    ids =  meta['id']
+
+    # features
+    acousticness = features[0]['acousticness']
+    danceability = features[0]['danceability']
+    energy = features[0]['energy']
+    instrumentalness = features[0]['instrumentalness']
+    liveness = features[0]['liveness']
+    valence = features[0]['valence']
+    loudness = features[0]['loudness']
+    speechiness = features[0]['speechiness']
+    tempo = features[0]['tempo']
+    key = features[0]['key']
+    time_signature = features[0]['time_signature']
+
+    track = [name, album, artist, ids, release_date, popularity, length, danceability, acousticness,
+            energy, instrumentalness, liveness, valence, loudness, speechiness, tempo, key, time_signature]
+    columns = ['name','album','artist','id','release_date','popularity','length','danceability','acousticness','energy','instrumentalness',
+                'liveness','valence','loudness','speechiness','tempo','key','time_signature']
+    return track,columns
+
+#return song signatures like general descriptions
+#we use the 4 most likely data that can affect our model's prediction accuracy
+def get_song_signatures(ids):
+    meta = sp.track(ids)
+    features = sp.audio_features(ids)
+
+    # meta
+    name = meta['name']
+    album = meta['album']['name']
+    artist = meta['album']['artists'][0]['name']
+    release_date = meta['album']['release_date']
+    length = meta['duration_ms']
+    popularity = meta['popularity']
+    ids =  meta['id']
+
+    # ML features
+    
+    danceability = features[0]['danceability']
+    energy = features[0]['energy']
+    valence = features[0]['valence']
+    loudness = features[0]['loudness']
+
+    track = [name, album, artist, ids, release_date, popularity, length, danceability, 
+            energy, valence, loudness]
+    columns = ['name','album','artist','id','release_date','popularity','length','danceability','energy',
+                'valence','loudness']
+    return track,columns
+
+#return list of song ids and artist ids
+def get_songs_artist_ids_playlist(ids):
+    playlist = sp.playlist_tracks(ids)
+    songs_id = []
+    artists_id = []
+    for result in playlist['items']:
+        songs_id.append(result['track']['id'])
+        for artist in result['track']['artists']:
+            artists_id.append(artist['id'])
+    return songs_id,artists_id
+
+# download playlist
+def download_playlist(id_playlist,n_songs):
+    songs_id = []
+    tracks = []
+
+    for i in range(0,n_songs,100):
+        playlist = spt.playlist_tracks(id_playlist,limit=100,offset=i)
+        
+        for songs in playlist['items']:
+            songs_id.append(songs['track']['id'])
+    
+    counter = 1
+    for ids in songs_id:
+        
+        time.sleep(.6)
+        track,columns = get_songs_features(ids)
+        tracks.append(track)
+
+        print(f"Song {counter} Added:")
+        print(f"{track[0]} By {track[2]} from the album {track[1]}")
+        clear_output(wait = True)
+        counter+=1
+    
+    clear_output(wait = True)
+    print("Music Downloaded!")
+
+    return tracks,columns
